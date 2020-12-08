97bb636f6370f81e900169027ae33b4504d371a8
Anna Lukianova <ann.lookyanova@gmail.com>
2020-12-08 19:32:24 +0100

    Move zeros better solution

--- moveZeros.js
+++ moveZeros.js
@@ -12,19 +12,26 @@
 //   }
 
 function moveZeroes(nums) {
-    let nonZeroCount = 0;
+    let insertAt = 0
 
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
-            [nums[nonZeroCount], nums[i]] = [nums[i], nums[nonZeroCount]]
-            // let temp = nums[zeroCount]
-            // nums[zeroCount] = nums[i]
-            // nums[i] = temp
-            nonZeroCount++
+        if (nums[i] < 0) {
+            [nums[insertAt], nums[i]] = [nums[i], nums[insertAt]]
+            insertAt++
         }
     }
+    for (let i = insertAt; i < nums.length; i++) {
+        if (nums[i] === 0) {
+            [nums[i], nums[insertAt] ] = [ nums[insertAt], nums[i]]
+            // let temp = nums[nonZeroCount]
+            // nums[nonZeroCount] = nums[i]
+            // nums[i] = temp
+            insertAt++
+        }
+    }
+ 
     return nums
 }
 
-console.log(moveZeroes([5,7,0,0,8]))
+console.log(moveZeroes([0, 5, 7, -6, 0, 0, 8, -20]))
 
