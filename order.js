83e2bd0bafd8676b7ba4099021ddeb96eae76890
Anna Lukianova <ann.lookyanova@gmail.com>
2021-06-24 16:53:17 +0200

    order

--- order.js
+++ order.js
@@ -16,4 +16,4 @@ function order(words) {
 const getNumber = str => parseInt(str.split('').find(isDigit))
 const isDigit = char => '0123456789'.includes(char)
 
-console.log(order("is2 Thi1s T4est 3a"))
+console.log(order("is2 Thi1s T4est 3a")) 
